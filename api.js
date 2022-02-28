4477c7d5f08d52200b51d252f989da32ae86b223
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2022-02-28 19:44:37 +0000

    Additional Changes to the Template + API Tests

---
+++ api.js
@@ -0,0 +1,14 @@
+function initMap() {  
+    const uluru = {
+        lat: 52.65396533743178,
+        lng: -0.48024596700849204
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: uluru,
+    }); 
+    const marker = new google.maps.Marker({    
+        position: uluru,
+        map: map,
+    });
+}
