9b5636e174780887b7f8ef239b45172a9339856e
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2022-04-22 00:47:37 +0100

    Submitted Website

--- api.js
+++ api.js
@@ -12,7 +12,3 @@ function initMap() {
         map: map,
     });
 }
-
-//       <div>
-//           <a class="twitter-timeline" data-lang="ja" data-theme="dark" href="https://twitter.com/Joseph_Brown_?ref_src=twsrc%5Etfw">Tweets by Joseph_Brown_</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-//       </div>
